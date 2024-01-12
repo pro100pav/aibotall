edb85932bc7b504095215b1f661ef287c147ac4a
Pavel <pasha.honor@yandex.ru>
2024-01-12 12:38:01 +0300

    create game

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,8 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/bots/*',
-        '/game/*',
         '/game',
+        '/bots/*',
+        '/game/bot/1',
+        
     ];
 }
