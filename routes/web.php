86ed89b5cc81ed640acb81a9034244e20808118b
Pavel <pasha.honor@yandex.ru>
2024-01-12 12:15:28 +0300

    create game

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::middleware(['auth','admin'])->prefix('admin')->group(function () {
 });
 
 Route::get('/game', [App\Http\Controllers\GameController::class, 'index'])->name('game');
-Route::get('/game/bot', [App\Http\Controllers\GameController::class, 'bot'])->name('game.bot');
+Route::post('/game/bot', [App\Http\Controllers\GameController::class, 'bot'])->name('game.bot');
 Route::fallback(function (){
     abort(404);
 });
